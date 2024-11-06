c264c1a7fc40bf83f259152ec45556386aabaa11
Parth215007 <parthc2903@gmail.com>
2024-11-06 15:07:53 +0530

    python

--- Python/maps/zip.py
+++ Python/maps/zip.py
@@ -9,4 +9,5 @@ print(list_4)
 
 stocks = ["reliance", "tcs", "apple"]
 prices = [1200,1450,2000]
-dict_1 = dict(stocks:prices, for stocks, prices in zip(stocks,prices))
+dict_1 = {stocks:prices for stocks, prices in zip(stocks,prices)}
+print(dict_1)
